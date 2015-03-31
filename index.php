1f19bee57cdd77b730cff37ab32935671ce98d69
haido <huuhai@jv-it.com.vn>
2015-03-31 11:17:18 +0700

    thêm magnific

--- index.php
+++ index.php
@@ -2,84 +2,72 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Design karaoke của tôi</title>
+    <title>Design bootstrap</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <ul class="thumbnails">
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Dân ca</h3>
-                            
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Nhạc đỏ</h3>
-                            
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Header Name</h3>
-                            
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Header Name</h3>
-                            
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Header Name</h3>
-                            
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
-                        <div class="caption">
-                            <h3>Header Name</h3>
 
-                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </ul>
-        </div>
+    <h5>Features:</h5>
+    <ul>
+        <li>Responsive design with hover effect</li>
+        <li>Compatible with bootstrap 3.0.0 and Up</li>
+        <li>No Javascript</li>
+    </ul>
+    <hr>
+
+    <div class="container">
+        <ul class="list-unstyled video-list-thumbs row">
+            <li class="col-lg-3 col-sm-4 col-xs-6">
+                <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                    <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                    <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                    <span class="glyphicon glyphicon-play-circle"></span>
+                    <span class="duration">03:15</span>
+                </a>
+                <h5>Nhạc tiếng Anh</h5>
+            </li>
+            <li class="col-lg-3 col-sm-4 col-xs-6">
+                <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                    <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                    <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                    <span class="glyphicon glyphicon-play-circle"></span>
+                    <span class="duration">03:15</span>
+                </a>
+                <h5>Nhạc tiếng Anh</h5>
+            </li>
+            <li class="col-lg-3 col-sm-4 col-xs-6">
+                <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                    <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                    <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                    <span class="glyphicon glyphicon-play-circle"></span>
+                    <span class="duration">03:15</span>
+                </a>
+                <h5>Nhạc tiếng Anh</h5>
+            </li>
+            <li class="col-lg-3 col-md-4 col-sm-4 col-xs-6">
+                <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                    <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                    <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                    <span class="glyphicon glyphicon-play-circle"></span>
+                    <span class="duration">03:15</span>
+                </a>
+                <h5>Nhạc tiếng Anh</h5>
+            </li>
+            <li class="col-lg-3 col-md-4 col-sm-4 col-xs-6">
+                <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                    <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                    <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                    <span class="glyphicon glyphicon-play-circle"></span>
+                    <span class="duration">03:15</span>
+                </a>
+                <h5>Nhạc tiếng Anh</h5>
+            </li>
+        </ul>
+
     </div>
 </body>
 </html>
