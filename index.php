28c1420b946f28012472fd81da235711ebde7ec3
haido <huuhai@jv-it.com.vn>
2015-03-31 09:08:50 +0700

    thêm mới

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Bootstrap 3 example</title>
+    <title>Design karaoke của tôi</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -72,7 +72,7 @@
                         <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
                         <div class="caption">
                             <h3>Header Name</h3>
-                            
+
                             <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
                             </p>
                         </div>
