c0491696589a914482488f7875fc714811e45354
haido <huuhai@jv-it.com.vn>
2015-03-31 08:59:40 +0700

    add theme

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Bootstrap 3 example</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <ul class="thumbnails">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img src="http://placehold.it/320x200" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <h3>Header Name</h3>
+                            
+                            <p align="center"><a href="http://bootsnipp.com/" class="btn btn-primary btn-block">Open</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
